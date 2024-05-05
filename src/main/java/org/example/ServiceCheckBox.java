628eadb3fce4ec636a68e5d64008618320fd7d02
Rustik123569 <rustam123569@mail.ru>
2024-05-05 20:31:16 +0500

    add ServiceCheckbox file

---
+++ src/main/java/org/example/ServiceCheckBox.java
@@ -0,0 +1,21 @@
+package org.example;
+import javax.swing.JCheckBox;
+
+public abstract class ServiceCheckBox extends JCheckBox {
+    private double cost;
+
+    public ServiceCheckBox(String text, double cost) {
+        super(text);
+        this.cost = cost;
+
+        this.addActionListener(e -> {
+            if (isSelected()) {
+                updateCost(text, cost);
+            } else {
+                updateCost(text, 0);
+            }
+        });
+    }
+
+    public abstract void updateCost(String text, double cost);
+}
