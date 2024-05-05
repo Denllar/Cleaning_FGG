79a22b8d984cf4e093dd84afc0ed0cc35e07543e
Denis Gabbasov <denisgabbasov06.09.2004@gmail.com>
2024-05-05 20:03:35 +0500

    add new file: CleaningServiceCalculator.java

---
+++ src/main/java/org/example/CleaningServiceCalculator.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javax.swing.*;
+
+public class CleaningServiceCalculator extends JFrame {
+    private JLabel totalCostLabel;
+    private double sofaCost = 0;
+    private double carpetsCost = 0;
+    private double windowsCost = 0;
+    private double kitchen = 0;
+    private double wall = 0;
+    private double complexProducts = 0;
+}
